1dbc9806eaee11553b56c0501faaef79100b63e8
Madhu Nadh Gurram <84019306+madhunadhgurram@users.noreply.github.com>
2025-03-21 20:34:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+
+resource "aws_instance" "madhu" {
+  ami           = "ami-04aa00acb1165b32a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "madhu-server"
+  }
+}
